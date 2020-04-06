6ff4b68c4c7a61c16ab81313776fd4dceaaeb0fd
Ben Parish <parishben13@gmail.com>
2020-04-06 03:49:31 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,8 +1,15 @@
 # Add your variables here
-def calculator(num1,num2!=0)
+def adder(num1,num2!=0)
   first_number = num1
   second_number = num2
-  sum = first_num + second_num
+  sum = first_number + second_number
   puts sum
   sum
+end
+def minus(num1,num2!=0)
+  first_number = num1
+  second_number = num2
+  difference = first_number - second_number
+  puts difference
+  difference
 end
