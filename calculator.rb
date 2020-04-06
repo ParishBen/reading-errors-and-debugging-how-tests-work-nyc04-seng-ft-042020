ae03eee5f6915830edacffe08c3c73ddfa67b098
Ben Parish <parishben13@gmail.com>
2020-04-06 03:54:45 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,12 +2,11 @@
   first_number = 22
   second_number = 33
   sum = first_number + second_number
-  puts sum
-  sum
+ 
 
 
-  first_number = 20
-  second_number = 10
-  difference = first_number - second_number
-  puts difference
-  difference
+  #first_number = 20
+  #second_number = 10
+  #difference = first_number - second_number
+  #puts difference
+  #difference
